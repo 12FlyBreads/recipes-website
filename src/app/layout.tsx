d661b5c8505c3611a2b1df567b5e4c9d4a1b2c93
12FlyBreads <arturgsimao@gmail.com>
2026-08-11 18:46:46 -0300

    feat: header structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ variable: "--font-inter", subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="pt-BR"
       className={`${inter.variable} antialiased`}
     >
+      <Header />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <header>
+            <Link href="/">Receitas Deliciosas</Link>
+            <nav>
+                <Link href="/">Início</Link>
+                <Link href="/receitas">Receitas</Link>
+            </nav>
+        </header>
+    )
+}
