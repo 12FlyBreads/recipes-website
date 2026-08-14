21ff2296f2ef93aa183b05bf18932ab99f8ada3b
12FlyBreads <arturgsimao@gmail.com>
2026-08-14 12:35:24 -0300

    feat: add style to the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="grow">
-      <div className="container mx-auto">
-      <section>
-        <h1>Receitas Deliciosas</h1>
-        <p>Descubra receitas simples e saborosas para todas as ocasiões.</p>
-        <Link href="/receitas">Ver todas as receitas</Link>
+      {/* seção hero */}
+      <section className="bg-orange-50 py-12">
+        <div className="flex flex-col items-center gap-6 container mx-auto">
+          <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
+          <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões.</p>
+          <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2" href="/receitas">Ver todas as receitas</Link>
+        </div>
       </section>
-      </div>
     </main>
   );
 }
