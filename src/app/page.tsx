98075fcfd104706516259082776701a3295b3390
12FlyBreads <arturgsimao@gmail.com>
2026-08-14 13:50:24 -0300

    feat: add style to highlight section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,13 @@ export default function Home() {
       </section>
 
       {/* seção de receitas em destaque */}
-      <section>
-        <div className="container mx-auto">
-          <h2>Receitas em Destaque</h2>
+      <section className="py-12">
+        <div className="container mx-auto flex flex-col items-center">
+          <h2 className="text-lg font-bold">Receitas em Destaque</h2>
 
           {/* cards */}
-          <Link href="/receitas">Ver todas as receitas<ChevronRightIcon /></Link>
+
+          <Link className="flex text-orange-400 hover:text-orange-700 transition-colors" href="/receitas">Ver todas as receitas<ChevronRightIcon /></Link>
         </div>
       </section>
     </main>
