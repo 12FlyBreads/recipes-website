14c4b8f8c752d49d925a3b4ce17c47697d1a17cd
12FlyBreads <arturgsimao@gmail.com>
2026-08-14 14:24:10 -0300

    feat: recipe page structure

---
+++ src/app/receitas/page.tsx
@@ -0,0 +1,13 @@
+export default function ReceitasPage() {
+    return (
+        <main className="grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todas as Receitas</h1>
+
+                <div>
+                    {/* array de receitas */}
+                </div>
+            </div>
+        </main>
+    )
+}
