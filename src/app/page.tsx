610033c57e17f8dfb4bb6a2a5b6b72ecb8efdb03
12FlyBreads <arturgsimao@gmail.com>
2026-08-14 12:36:45 -0300

    feat: button hover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="flex flex-col items-center gap-6 container mx-auto">
           <h1 className="text-5xl font-bold">Receitas Deliciosas</h1>
           <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões.</p>
-          <Link className="bg-orange-500 text-white font-bold rounded-lg px-3 py-2" href="/receitas">Ver todas as receitas</Link>
+          <Link className="bg-orange-500 hover:bg-orange-700 transition-colors text-white font-bold rounded-lg px-3 py-2" href="/receitas">Ver todas as receitas</Link>
         </div>
       </section>
     </main>
