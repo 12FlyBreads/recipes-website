68ccd356fb988bd80b3ca6689680c0a25d3d5790
12FlyBreads <arturgsimao@gmail.com>
2026-08-16 15:43:57 -0300

    feat: recipe modal

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -1,11 +1,24 @@
+"use client"
+
 import RecipeCard from "@/components/RecipeCard";
+import RecipeFormModal from "@/components/RecipeFormModal";
 import { recipes } from "@/lib/data";
+import { Plus } from "lucide-react";
+import { useState } from "react";
 
 export default function ReceitasPage() {
+    const [isRecipeModalOpen, setIsRecipeModalOpen] = useState(false);
+
     return (
         <main className="grow py-8">
             <div className="container mx-auto">
-                <h1 className="text-3xl font-bold">Todas as Receitas</h1>
+                <div className="flex justify-between w-full">
+                    <h1 className="text-3xl font-bold">Todas as Receitas</h1>
+                    <button onClick={() => setIsRecipeModalOpen(true)} className="flex items-center gap-2 px-4 py-2 rounded-lg text-white hover:bg-gray-800 transition-colors bg-black">
+                        <Plus size={16}/>
+                        Nova Receita
+                    </button>
+                </div>
 
                 <div className="grid grid-cols-3 gap-8 mt-8">
                     { recipes.map((recipe) => 
@@ -13,6 +26,7 @@ export default function ReceitasPage() {
                     }
                 </div>
             </div>
+            <RecipeFormModal isOpen={isRecipeModalOpen} onClose={() => setIsRecipeModalOpen(false)} />
         </main>
     )
 }
