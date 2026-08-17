d20050c9f23eb62b76179ebe265b7fde57cbe80d
12FlyBreads <arturgsimao@gmail.com>
2026-08-16 22:03:14 -0300

    feat: RWF and yup setup

--- src/components/RecipeFormModal/index.tsx
+++ src/components/RecipeFormModal/index.tsx
@@ -1,4 +1,8 @@
+import { useForm } from "react-hook-form";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "../ui/dialog";
+import { RecipeFormData, recipeSchema } from "@/lib/formValidationSchemas/recipeSchema";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 
 interface RecipeFormModalProps {
     isOpen: boolean;
@@ -6,6 +10,21 @@ interface RecipeFormModalProps {
 }
 
 export default function RecipeFormModal({ isOpen, onClose }:RecipeFormModalProps) {
+    const {
+        register,
+        reset,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<RecipeFormData>({
+        resolver: yupResolver(recipeSchema),
+        mode: "onSubmit"
+    })
+
+    const onSubmit = (data: RecipeFormData) => {
+        console.log(data);
+        reset();
+    }
+
     const inputStyle="p-2 border border-zinc-200 rounded-md";
 
     return (
@@ -14,37 +33,44 @@ export default function RecipeFormModal({ isOpen, onClose }:RecipeFormModalProps
                 <DialogHeader>
                     <DialogTitle>Nova Receita</DialogTitle>
                 </DialogHeader>
-                <form className="flex flex-col gap-4 w-full">
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-full">
                     <div className="grid grid-cols-2 gap-2">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="title">Titulo</label>
-                            <input className={inputStyle} type="text" id="title"></input>
+                            <input className={inputStyle} type="text" id="title" {...register("title")}></input>
+                            {errors.title ? <span className="text-sm text-red-500">{errors.title.message}</span> : null}
                         </div>
                         <div className="flex flex-col gap-1">
                             <label htmlFor="category">Categoria</label>
-                            <input className={inputStyle} type="text" id="category"></input>
+                            <input className={inputStyle} type="text" id="category" {...register("category")}></input>
+                            {errors.category ? <span className="text-sm text-red-500">{errors.category.message}</span> : null}
                         </div>
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="description">Descrição</label>
-                        <textarea className={inputStyle} id="description"></textarea>
+                        <textarea className={inputStyle} id="description" {...register("description")}></textarea>
+                        {errors.description ? <span className="text-sm text-red-500">{errors.description.message}</span> : null}
                     </div>
                     <div className="flex flex-col gap-1">
                         <label htmlFor="imageUrl">URL da Imagem</label>
-                        <input type="text" className={inputStyle} id="imageUrl" placeholder="https://example.com/image.jpg"></input>
+                        <input type="text" className={inputStyle} id="imageUrl" placeholder="https://example.com/image.jpg" {...register("image")}></input>
+                        {errors.image ? <span className="text-sm text-red-500">{errors.image.message}</span> : null}
                     </div>
                     <div className="grid grid-cols-3 gap-2">
                         <div className="flex flex-col gap-1">
                             <label htmlFor="prepTime">Tempo de preparo</label>
-                            <input className={inputStyle} type="text" id="prepTime" placeholder="15 minutos"></input>
+                            <input className={inputStyle} type="text" id="prepTime" placeholder="15 minutos" {...register("prepTime")}></input>
+                            {errors.prepTime ? <span className="text-sm text-red-500">{errors.prepTime.message}</span> : null}
                         </div>
                         <div className="flex flex-col gap-1">
                             <label htmlFor="cookTime">Tempo de cozimento</label>
-                            <input className={inputStyle} type="text" id="cookTime" placeholder="30 minutos"></input>
+                            <input className={inputStyle} type="text" id="cookTime" placeholder="30 minutos" {...register("cookTime")}></input>
+                            {errors.cookTime ? <span className="text-sm text-red-500">{errors.cookTime.message}</span> : null}
                         </div>
                         <div className="flex flex-col gap-1">
                             <label htmlFor="servings">Porções</label>
-                            <input className={inputStyle} type="number" id="servings" defaultValue="1"></input>
+                            <input className={inputStyle} type="number" id="servings" defaultValue="1" {...register("servings")}></input>
+                            {errors.servings ? <span className="text-sm text-red-500">{errors.servings.message}</span> : null}
                         </div>
                     </div>
                     <div className="flex gap-2 self-end">
---
+++ src/lib/formValidationSchemas/recipeSchema.ts
@@ -0,0 +1,13 @@
+import * as yup from "yup";
+
+export const recipeSchema = yup.object().shape({
+    title: yup.string().required("O título é obrigatório"),
+    category: yup.string().required("A categoria é obrigatória"),
+    description: yup.string().required("A descrição é obrigatória"),
+    image: yup.string().required("A URL da imagem é obrigatória"),
+    prepTime: yup.string().required("O tempo de preparo é obrigatório"),
+    cookTime: yup.string().required("O tempo de cozimento é obrigatório"),
+    servings: yup.number().typeError("As porções devem ser um número").positive("As porções devem ser um número positivo").integer("As porções devem ser um número inteiro").min(1, "As porções devem ser pelo menos 1").required("As porções são obrigatórias")
+});
+
+export type RecipeFormData = yup.InferType<typeof recipeSchema>;
